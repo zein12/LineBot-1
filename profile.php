e7de6ab33033de8e943e212897382f764ac7cadf
CozaLufy <science_am@hotmail.com>
2016-11-15 13:03:31 +0700

    change url

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
     $channelID = "1488211839"; 
     $channelSecret = "3eec2a0e5a022b191d8f90330fbcaa20"; 
     $channelMID = "uf714a5b008cca057a4cb8cf7c0cb2e6c";
-    $url  =  "https://trialbot-api.line.me/v1/profiles?mids={$channelMID}" ; 
+    $url  =  "https://trialbot-api.line.me/v1/profiles" ; 
     $headers = array(
       "Content-Type: application/json; charser=UTF-8",
       "X-Line-ChannelID: " . $channelID,
@@ -11,10 +11,8 @@
     );
     
     $ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		$result = curl_exec($ch);
 		curl_close($ch);
 
