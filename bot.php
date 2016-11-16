f9dd558f5f5ef765734c98bf2d5d4f7232647e7a
CozaLufy <science_am@hotmail.com>
2016-11-16 13:12:06 +0700

    .

--- bot.php
+++ bot.php
@@ -63,7 +63,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $tevent['message']['text'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
@@ -94,6 +94,30 @@ if (!is_null($events['events'])) {
 			echo $result . "\r\n";
 		}
 	}
+}else{
+  $url = 'https://api.line.me/v2/bot/message/push';
+  $to = $events['result']['to'];
+  $messages = [
+  				'type' => 'text',
+  				'text' => "Hello"
+  			];
+  $data = [
+  	'to' => $to,
+  	'messages' => [$messages],
+  ];
+  $post = json_encode($data);
+  $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+  
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  
+  echo $result . "\r\n";
 }
 
 /* 
