a242ad5f7be4d9406784d595f4ff86fb8a7063dc
CozaLufy <science_am@hotmail.com>
2016-11-16 17:20:30 +0700

    .

--- bot.php
+++ bot.php
@@ -74,25 +74,25 @@ if (!is_null($events['events'])) {
 			]; 
       */
       $actions =  [{
-         'type' : 'postback',
-         'label' :"test button postback"
-         'data' : "item=123"
+         'type' => 'postback',
+         'label' =>"test button postback"
+         'data' => "item=123"
          }
       ];
       
       
-      $messages = {        
-        'type' : 'template',
-				'allText' : 'this is an template',
-        'template' : {
-           'type' : 'buttons',
-    				'thumbnailImageUrl' : 'http://1.bp.blogspot.com/_VChD0TN44Cc/S8nYSoHXeQI/AAAAAAAAIrg/frnUGRABF2w/s400/3.1.jpg',
-            'title' : 'test button',
-            'text' :  $event['message']['text'],
+      $messages = [        
+        'type' => 'template',
+				'allText' => 'this is an template',
+        'template' => [
+           'type' => 'buttons',
+    				'thumbnailImageUrl' => 'http://1.bp.blogspot.com/_VChD0TN44Cc/S8nYSoHXeQI/AAAAAAAAIrg/frnUGRABF2w/s400/3.1.jpg',
+            'title' => 'test button',
+            'text' =>  $event['message']['text'],
             'actions' => $actions  
-        },
+        ],
         
-			};   	 	
+			];   	 	
 			
 		} else if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {  
          // Build message to reply back
--- profile.php
+++ profile.php
@@ -35,3 +35,33 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
+
+$actions =  [{
+         'type' => 'postback',
+         'label' =>"test button postback"
+         'data' => "item=123"
+         }
+      ];
+      
+      
+      $messages = [        
+        'type' => 'template',
+				'allText' => 'this is an template',
+        'template' => [
+           'type' => 'buttons',
+    				'thumbnailImageUrl' => 'http://1.bp.blogspot.com/_VChD0TN44Cc/S8nYSoHXeQI/AAAAAAAAIrg/frnUGRABF2w/s400/3.1.jpg',
+            'title' => 'test button',
+            'text' =>  'event message text',
+            'actions' => $actions  
+        ],
+        
+			];
+      
+      $data = [
+				'replyToken' => 'replyToken',
+				'messages' => [$messages],
+			];
+      
+      $post = json_encode($data);
+      
+      echo $post;
