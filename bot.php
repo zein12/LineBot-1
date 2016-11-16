dda1daf4b9a85bd604216fc52ad42bdf2baf8285
CozaLufy <science_am@hotmail.com>
2016-11-16 15:25:20 +0700

    .

--- bot.php
+++ bot.php
@@ -58,24 +58,34 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);  
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
+  $url = 'https://api.line.me/v2/bot/message/reply';
 	// Loop through each event
 	foreach ($events['events'] as $event) {
+      $replyToken = $event['replyToken'];
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => $text." ".json_encode($events)
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
+			];   	
+			
+		} else if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {  
+         // Build message to reply back
+  			$messages = [
+  				'type' => 'sticker',
+  				'packageId' => "3",
+          'stickerId' => "183"
+  			];  
+  		  
+    }
+    
+    
+   // Make a POST Request to Messaging API to reply to sender   
+    if (!is_null($messages)) {
+      $data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
@@ -92,35 +102,6 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-		} elseif ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
-         $replyToken = $event['replyToken'];
-
-  			// Build message to reply back
-  			$messages = [
-  				'type' => 'sticker',
-  				'packageId' => "3",
-          'stickerId' => "183"
-  			];
-  
-  			// Make a POST Request to Messaging API to reply to sender
-  			$url = 'https://api.line.me/v2/bot/message/reply';
-  			$data = [
-  				'replyToken' => $replyToken,
-  				'messages' => [$messages],
-  			];
-  			$post = json_encode($data);
-  			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-  
-  			$ch = curl_init($url);
-  			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-  			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-  			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  			$result = curl_exec($ch);
-  			curl_close($ch);
-  
-  			echo $result . "\r\n";
     }
 	}
 }
