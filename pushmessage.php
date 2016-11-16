18cd5e3c76bb04e1dedeb4ff57c487ab1b4003a1
CozaLufy <science_am@hotmail.com>
2016-11-16 11:03:40 +0700

    .

--- pushmessage.php
+++ pushmessage.php
@@ -2,9 +2,13 @@
 
 $access_token = '/uRUSV5cXcYdnAjK7n16+BE9EavYwZay0E3zYt340wH+E3J95IwzSPT++IDf6tHTxHlDW1Az0IVwi7pqjfIAza+J0qRA+7+1nzAIZN1JEx1Ly8KSNXXY1pKm8VFpWLbdNy3iwH6cH4fchucMF16kNAdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v2/bot/message/push';
+$messages = [
+				'type' => 'text',
+				'text' => "test send message push"
+			];
 $data = [
-	'to' => $replyToken,
-	'messages' => "test push",
+	'to' => "Uc23982bf348aa387c2b73bcb2051a709",
+	'messages' => [$messages],
 ];
 $post = json_encode($data);
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
