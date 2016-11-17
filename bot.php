af77903f70c16b84101c93b7ac2561630478fe30
CozaLufy <science_am@hotmail.com>
2016-11-17 15:26:59 +0700

    .

--- bot.php
+++ bot.php
@@ -99,15 +99,14 @@ if (!is_null($events['events'])) {
                 'thumbnailImageUrl: https://innova-linebot.herokuapp.com/69686.jpg'
              );
        */
-       $actions_view = [{
-                 'type' => 'uri',
-                 'label' =>'View detail',
-                 'uri': 'http://s1.tsuki-board.net/pics/figure/big/69686.jpg?t=1340402295' 
-              }];
+       $actions_view = array(
+                'type: buttons',
+                'thumbnailImageUrl: https://innova-linebot.herokuapp.com/69686.jpg'
+             );
        
        $template_view = [
         'type' => 'buttons',
-        'actions' => $actions_view
+        'actions' => [$actions_view]
         ];
        $messages_view = [        
         'type' => 'template',
