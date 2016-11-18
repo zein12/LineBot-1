e0dd14325f17c0e99108315086ff865f0e1ff93a
CozaLufy <science_am@hotmail.com>
2016-11-18 11:34:39 +0700

    .

--- callback.php
+++ callback.php
@@ -90,6 +90,23 @@ if (!is_null($events['events'])) {
                   break;                          
               
           }
+          
+          $url = 'https://dice.in.th/LineBot/manage_data.php';
+        $data = [
+          				'event' => $event['type'],
+          				'data' => $messages,
+          			];
+        $post = json_encode($data);
+  			$headers = array('Content-Type: application/json');
+  
+  			$ch = curl_init($url);
+  			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+  			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  			$result = curl_exec($ch);
+  			curl_close($ch);
       
           if($event['source']['type'] != "room") {
               replyMessage($replyToken, $messages);
