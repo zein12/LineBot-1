47b7396fbdfc5a28d541a23450b378a4cbfebdb9
CozaLufy <science_am@hotmail.com>
2016-11-21 16:18:16 +0700

    .

--- profile.php
+++ profile.php
@@ -105,7 +105,7 @@ $url = 'https://api.line.me/v1/profile/{'.$user.'}';
       
     $channelID = "1488211839"; 
     $channelSecret = "3eec2a0e5a022b191d8f90330fbcaa20"; 
-    $channelMID = "uf714a5b008cca057a4cb8cf7c0cb2e6c";      
+    $channelMID = "Uc23982bf348aa387c2b73bcb2051a709";      
     $url  =  "https://api.line.me/v2/bot/profile?userId=".$channelMID; 
     $access_token = '/uRUSV5cXcYdnAjK7n16+BE9EavYwZay0E3zYt340wH+E3J95IwzSPT++IDf6tHTxHlDW1Az0IVwi7pqjfIAza+J0qRA+7+1nzAIZN1JEx1Ly8KSNXXY1pKm8VFpWLbdNy3iwH6cH4fchucMF16kNAdB04t89/1O/w1cDnyilFU=';
     echo $url;
