b65bac34d8a009e4662ab26cca93ec7ea1fd4724
CozaLufy <science_am@hotmail.com>
2016-11-21 10:57:33 +0700

    .

--- callback.php
+++ callback.php
@@ -8,6 +8,7 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
   //$url = 'https://api.line.me/v2/bot/message/reply';
 	// Loop through each event
+  /*
 	foreach ($events['events'] as $event) {
       $replyToken = $event['replyToken']; 
       $messages = [        
@@ -16,7 +17,13 @@ if (!is_null($events['events'])) {
     			];
           
       replyMessage($replyToken, $messages);      
-  }
+  } 
+  */
+  $messages = [        
+            'type' => 'text',
+    				'text' => json_encode($events)
+    			];
+  pushMessage("Uc23982bf348aa387c2b73bcb2051a709", $messages);
 }
 
 function replyMessage($replyToken, $data_messages) {
@@ -39,7 +46,29 @@ function replyMessage($replyToken, $data_messages) {
 			curl_close($ch);
 
 			echo $result . "\r\n";    
-}
+} 
+
+function pushMessage($to, $data_messages) {
+      $access_token = '/uRUSV5cXcYdnAjK7n16+BE9EavYwZay0E3zYt340wH+E3J95IwzSPT++IDf6tHTxHlDW1Az0IVwi7pqjfIAza+J0qRA+7+1nzAIZN1JEx1Ly8KSNXXY1pKm8VFpWLbdNy3iwH6cH4fchucMF16kNAdB04t89/1O/w1cDnyilFU='; 
+      $url = 'https://api.line.me/v2/bot/message/push';
+      $data = [
+        				'to' => $to,
+        				'messages' => [$data_messages],
+        			];
+      $post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";    
+}  
 
 function LeaveRequest($leaveType, $id) {
       $access_token = '/uRUSV5cXcYdnAjK7n16+BE9EavYwZay0E3zYt340wH+E3J95IwzSPT++IDf6tHTxHlDW1Az0IVwi7pqjfIAza+J0qRA+7+1nzAIZN1JEx1Ly8KSNXXY1pKm8VFpWLbdNy3iwH6cH4fchucMF16kNAdB04t89/1O/w1cDnyilFU='; 
