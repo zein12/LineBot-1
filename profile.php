dc44bdf734bc4bc0593ef5e27336ceaa98c42301
CozaLufy <science_am@hotmail.com>
2016-11-21 15:37:42 +0700

    .

--- profile.php
+++ profile.php
@@ -88,7 +88,7 @@ function botmain($json){
   }
 }
 */
-
+/*
 $access_token = '/uRUSV5cXcYdnAjK7n16+BE9EavYwZay0E3zYt340wH+E3J95IwzSPT++IDf6tHTxHlDW1Az0IVwi7pqjfIAza+J0qRA+7+1nzAIZN1JEx1Ly8KSNXXY1pKm8VFpWLbdNy3iwH6cH4fchucMF16kNAdB04t89/1O/w1cDnyilFU=';
  $user = "Uc23982bf348aa387c2b73bcb2051a709";
 $url = 'https://api.line.me/v1/profile/{'.$user.'}';
@@ -101,3 +101,24 @@ $url = 'https://api.line.me/v1/profile/{'.$user.'}';
       curl_close($ch);
       $ch_result = json_decode($result, true);
       var_dump($ch_result);
+      */
+      
+    $channelID = "1488211839"; 
+    $channelSecret = "3eec2a0e5a022b191d8f90330fbcaa20"; 
+    $channelMID = "uf714a5b008cca057a4cb8cf7c0cb2e6c";      
+    $url  =  "https://trialbot-api.line.me/v1/profiles?mids={$channelMID}" ; 
+    $headers = array(
+      "Content-Type: application/json; charser=UTF-8",  
+      "X-Line-ChannelID: " . $channelID,
+      "X-Line-ChannelSecret: " . $channelSecret,
+      "X-Line-Trusted-User-With-ACL: " . $channelMID
+    ); 
+    
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+		echo $result . "\r\n";
