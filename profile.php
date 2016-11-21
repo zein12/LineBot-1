6acedbe602165c92843aa0584a4ad089dd4e265a
CozaLufy <science_am@hotmail.com>
2016-11-21 15:44:27 +0700

    .

--- profile.php
+++ profile.php
@@ -106,19 +106,14 @@ $url = 'https://api.line.me/v1/profile/{'.$user.'}';
     $channelID = "1488211839"; 
     $channelSecret = "3eec2a0e5a022b191d8f90330fbcaa20"; 
     $channelMID = "uf714a5b008cca057a4cb8cf7c0cb2e6c";      
-    $url  =  "https://trialbot-api.line.me/v1/profiles?mids={$channelMID}" ; 
-    $headers = array(
-      "Content-Type: application/json; charser=UTF-8",  
-      "X-Line-ChannelID: " . $channelID,
-      "X-Line-ChannelSecret: " . $channelSecret,
-      "X-Line-Trusted-User-With-ACL: " . $channelMID
-    ); 
-    
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-
-		echo $result . "\r\n";
+    $url  =  "https://api.line.me/v2/bot/profile/{$channelMID}" ; 
+    echo $url;
+    $headers = array('Authorization: Bearer ' . $access_token);        
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
+      $ch_result = json_decode($result, true);
+      var_dump($ch_result);
