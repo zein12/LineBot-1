e69bddd1f4b26f76a590eeb760cc1e3ec08c616c
CozaLufy <science_am@hotmail.com>
2016-11-22 12:13:13 +0700

    .

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 $access_token = '/uRUSV5cXcYdnAjK7n16+BE9EavYwZay0E3zYt340wH+E3J95IwzSPT++IDf6tHTxHlDW1Az0IVwi7pqjfIAza+J0qRA+7+1nzAIZN1JEx1Ly8KSNXXY1pKm8VFpWLbdNy3iwH6cH4fchucMF16kNAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -14,8 +14,9 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
-*/
+/*
 
 $json = file_get_contents("https://dice.in.th/LineBot/friends_list.json");
 $obj = json_decode($json, true);
 var_dump($obj);
+*/
