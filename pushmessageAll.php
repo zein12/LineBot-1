5187384d2e6182556d84df66eccc0aef6f1f26b5
CozaLufy <science_am@hotmail.com>
2016-11-22 10:57:41 +0700

    .

--- pushmessageAll.php
+++ pushmessageAll.php
@@ -1,33 +1,35 @@
 <?php  
-/*      
+     
 $access_token = '/uRUSV5cXcYdnAjK7n16+BE9EavYwZay0E3zYt340wH+E3J95IwzSPT++IDf6tHTxHlDW1Az0IVwi7pqjfIAza+J0qRA+7+1nzAIZN1JEx1Ly8KSNXXY1pKm8VFpWLbdNy3iwH6cH4fchucMF16kNAdB04t89/1O/w1cDnyilFU='; 
 $url = 'https://api.line.me/v2/bot/message/push';   
       
 $json = file_get_contents("https://dice.in.th/LineBot/friends_list.json");
 $obj = json_decode($json, true);
 $arr_data = array(); 
-foreach($obj as $rs) {
-  array_push($arr_data, $rs["userId"]); 
+$messages = array('type' => 'text',	'text' => "Hi.." );
+foreach($obj as $rs) {   
+    $data = array('to' => $rs["userId"], 'messages' => $messages);
+    array_push($arr_data, $data); 
 }
 //var_dump($arr_data);
-$jsondata = json_encode($arr_data);
+ //var_dump(json_encode($arr_data));
 //var_dump($jsondata);
-
+ /* 
 $messages = [
 				'type' => 'text',
 				'text' => "Hi.."
 			];
      
 $data = [
-	'to' => ["Uc23982bf348aa387c2b73bcb2051a709","U9064a2310b6a52cdbb0682912ba6179c"],
+	'to' => $rs["userId"],
 	'messages' => [$messages]
 ];
- 
+ */
 //var_dump($data);
-$post = json_encode($data);
+$post = json_encode($arr_data);
 //var_dump($post);
 
-  
+ 
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
@@ -41,7 +43,7 @@ curl_close($ch);
 //$ch_result = json_decode($result, true);
 echo $result;
 */
-
+ /*
     $channelID = "1488211839"; 
     $channelSecret = "3eec2a0e5a022b191d8f90330fbcaa20"; 
     $channelMID = "uf714a5b008cca057a4cb8cf7c0cb2e6c";
@@ -78,5 +80,6 @@ $result = curl_exec($ch);
 curl_close($ch);
 //$ch_result = json_decode($result, true);
 echo $result;
+*/
 
  
