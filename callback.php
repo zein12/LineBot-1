18c73a2117e0eb23064dd6994510dac6599dda19
CozaLufy <science_am@hotmail.com>
2016-11-23 14:30:40 +0700

    .

--- callback.php
+++ callback.php
@@ -9,12 +9,84 @@ if (!is_null($events['events'])) {
 	// Loop through each event  
 	foreach ($events['events'] as $event) {
       $replyToken = $event['replyToken']; 
-      $messages = [        
-            'type' => 'text',
-    				'text' => json_encode($events)
-    			];
-          
-          replyMessage($replyToken, $messages);      
+      if($event['type'] == 'follow') {
+         /*
+         {"events":[
+          {"type":"follow",
+          "replyToken":"2b34541c919a46179f4f81e3b9ea6588",
+          "source":{"userId":"Uc23982bf348aa387c2b73bcb2051a709","type":"user"},
+          "timestamp":1479374241667}]}
+      */       
+        
+          $url = 'https://dice.in.th/LineBot/manage_data.php';
+          $data = [
+            				'events' => $event['type'],
+            				'userId' => $event['source']['userId'],
+            			];
+          $post = json_encode($data);
+    			$headers = array('Content-Type: application/json');
+    
+    			$ch = curl_init($url);
+    			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    			$result = curl_exec($ch);
+    			curl_close($ch);
+       
+       // Reply only when message sent 
+		  } else if($event['type'] == 'message') {
+         
+          switch ($event['message']['type']) {
+              case "text":
+                  $text = $event['message']['text'];
+                  if($text == 'shop'){
+                      $actions_view = array(
+                      		'type' => 'postback' ,
+                          'label' =>'Buy',
+                          'data'=> 'action=buy&itemid=123'
+                      	);
+                       $template_view = [
+                        'type' => 'buttons',
+                        'thumbnailImageUrl' => 'https://innova-linebot.herokuapp.com/69686.jpg',
+                        'title' => 'Description',
+                        'text' =>  'cartoon one piece',
+                        'actions' => [$actions_view]
+                        ];
+                       $messages = [        
+                        'type' => 'template',
+                				'altText' => 'this is an template',
+                        'template' => $template_view
+                        ];
+                  }else{
+                      $messages = [        
+                        'type' => 'text',
+                				'text' => $text    
+                			]; 
+                  }
+                  break; 
+              case "sticker":
+                  $messages = [
+            				'type' => 'sticker',
+            				'packageId' => "3",
+                    'stickerId' => "183"            
+            			];
+                  break;
+              default:
+                  $messages = [
+            				'type' => 'sticker',
+            				'packageId' => "1",
+                    'stickerId' => "5"            
+            			]; 
+                  break;                      
+          }           
+        
+          if($event['source']['type'] != "room") {
+              replyMessage($replyToken, $messages);
+          }
+      // action postback
+      }    
       
   } 
 }
