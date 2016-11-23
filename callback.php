0c96c3d8c6862ebf2a8971e96e4d6114c0043942
CozaLufy <science_am@hotmail.com>
2016-11-23 14:03:00 +0700

    .

--- callback.php
+++ callback.php
@@ -15,18 +15,11 @@ if (!is_null($events['events'])) {
                 				'text' => $text." ".json_encode($events)    
                 			]; 
                       
-       replyMessage($replyToken, $messages);
-       
-      
-  }
-}
-
-function replyMessage($replyToken, $data_messages) {
       $access_token = '/uRUSV5cXcYdnAjK7n16+BE9EavYwZay0E3zYt340wH+E3J95IwzSPT++IDf6tHTxHlDW1Az0IVwi7pqjfIAza+J0qRA+7+1nzAIZN1JEx1Ly8KSNXXY1pKm8VFpWLbdNy3iwH6cH4fchucMF16kNAdB04t89/1O/w1cDnyilFU='; 
       $url = 'https://api.line.me/v2/bot/message/reply';
       $data = [
         				'replyToken' => $replyToken,
-        				'messages' => [$data_messages],
+        				'messages' => [$messages],
         			];
       $post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
@@ -40,6 +33,11 @@ function replyMessage($replyToken, $data_messages) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 
-			echo $result . "\r\n";    
-}  
+			echo $result . "\r\n";
+       
+      
+  }
+}
+
+
 
