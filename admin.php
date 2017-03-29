6c870537b2f8b820da4b5359cdf5e997c9078928
zein12 <jena.zein@gmail.com>
2017-03-30 06:18:10 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -132,7 +132,7 @@ body{
   $json = file_get_contents("https://dice.in.th/LineBot/friends_list.json");
   $obj = json_decode($json, true);
   //var_dump($obj);
-  $access_token = '/uRUSV5cXcYdnAjK7n16+BE9EavYwZay0E3zYt340wH+E3J95IwzSPT++IDf6tHTxHlDW1Az0IVwi7pqjfIAza+J0qRA+7+1nzAIZN1JEx1Ly8KSNXXY1pKm8VFpWLbdNy3iwH6cH4fchucMF16kNAdB04t89/1O/w1cDnyilFU=';  
+  $access_token = '3/cEBpOR0mjAMUtnHKrSrx3N6FnMVNPYfXBIwMO6HNGaljxuxTxZz2fGrmZYFwqfV3dvAWMa7FEGrmOONfbZ7or1wxYgpjbtFMS0Mkk+RftjvYSrUpThxAHGiivf2M662z2zM5P8BSKby0dJiBG3GQdB04t89/1O/w1cDnyilFU=';  
  
   foreach($obj as $rs) {       
       $url  =  "https://api.line.me/v2/bot/profile/".$rs["userId"];     
